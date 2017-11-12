97ba9b1bd1dc7d7c56670e2232ee4618777049f2
Minyi Soon <rocky@Minyis-MBP-2.localdomain>
2017-11-12 17:44:57 +0000

    serve html

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', express.static('browser'))
 
 app.listen(process.env.PORT, () => console.log('Example app listening on port 80!'))
